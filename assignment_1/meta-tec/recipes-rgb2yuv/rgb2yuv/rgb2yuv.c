4139d29ee585a26e5468c8e39d71baeb5cc7b2e7
Edward <edwarduw@gmail.com>
2019-06-22 18:54:49 -0600

    Added recipe and meta layers

---
+++ assignment_1/meta-tec/recipes-rgb2yuv/rgb2yuv/rgb2yuv.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+double convertNumbers[3][3] = {{0.299,0.587,0.114},{-0.14713, -0.28886, 0.436},{0.615, -0.51499, -0.10001}};
+
+void rgb2yuv(char *input_image, char *output_image)
+{
+   if(outputFile == NULL || !inputFile == NULL) 
+   {
+      printf("Set output and input file\n");
+      return;
+   }
+
+   FILE* inputFile;
+   inputFile = fopen(input_image, "r");
+   if(inputFile == NULL){
+      printf("Wrong file path, could not load input image");
+      return;
+   }
+
+
+}
+
+
+int main (int argc, char **argv)
+{
+   char *outputFile = NULL;
+   char *inputFile = NULL;
+
+   int c;
+   while ((c = getopt (argc, argv, ":i:o:ha")) != -1)
+   {
+   	switch(c)
+   	{
+   		case 'i':
+   		{
+   			inputFile = optarg;
+   			printf("Input file: %s\n", inputFile); 
+   			break;
+   		}
+   		case 'o':
+   		{
+   			outputFile = optarg;
+   			printf("Output file %s\n", outputFile);
+   			break;
+   		}
+   		case 'h':
+   		{
+   			printf("Help file\n");
+   			break;
+   		}
+   		case 'a':
+   		{
+   			printf("Authors:\n Luis Arias \n Guillermo Lopez \n Edward Umana\n");
+   			break;
+   		}
+
+
+   	}
+   }
+   return 0;
+}
