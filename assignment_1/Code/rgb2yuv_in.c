b5ef6c48f4c025375e51f91595622ce018def030
luigi1801 <luigianargo@gmail.com>
2019-06-25 19:07:28 -0600

    rgb2yuv_intrinsics and results documentation

--- assignment_1/Code/rgb2yuv_in.c
+++ assignment_1/Code/rgb2yuv_in.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <arm_neon.h>
 
 #define SCALE_U         0.436*2
 #define SCALE_V         0.615*2
@@ -10,35 +11,41 @@
 #define OFFSET_UV       128
 #define HEIGHT          480
 #define WIDTH           640
-
+#define Y               0
+#define U               1
+#define V               2
 
 struct b_pixelRGB{
-   unsigned char b;
-   unsigned char g;
-   unsigned char r;
+   uint8_t b;
+   uint8_t g;
+   uint8_t r;
 };
 
 struct f_pixelRGB{
-   float b;
-   float g;
-   float r;
+   float32_t r;
+   float32_t g;
+   float32_t b;
+   float32_t z;
 };
 
 struct b_pixelYUV{
-   unsigned char y;
-   unsigned char u;
-   unsigned char v;
+   uint8_t y;
+   uint8_t u;
+   uint8_t v;
 };
 
 struct f_pixelYUV{
-   float y;
-   float u;
-   float v;
+   float32_t y;
+   float32_t u;
+   float32_t v;
+   float32_t z;
 };
 
-double convertNumbers[3][3] = {{0.299,0.587,0.114},{-0.14713, -0.28886, 0.436},{0.615, -0.51499, -0.10001}};
+float32_t convertNumbers[3][4] = {{0.299,0.587,0.114,0},{-0.14713, -0.28886, 0.436,0},{0.615, -0.51499, -0.10001,0}};
+//float32x2_t coefs[3][2] = {{0.299,0.587,0.114,0},{-0.14713, -0.28886, 0.436,0},{0.615, -0.51499, -0.10001,0}};
+float32x2_t coefs[3][2] = {{{0.299,0.587},{0.114,0}},{{-0.14713, -0.28886}, {0.436,0}},{{0.615, -0.51499}, {-0.10001,0}}};
 
-float to8bits(float val)
+float32_t to8bits(float32_t val)
 {
    if (val < 0)
    {
@@ -56,6 +63,7 @@ void rgb2yuv(char *input_image, char *output_image)
    int i = 0;
    int c = 0;
    int r = 0;
+   uint32x2_t tmp;
 
    // Validaciones de entrada
    if(input_image == NULL || output_image == NULL) 
@@ -74,14 +82,26 @@ void rgb2yuv(char *input_image, char *output_image)
    // Reservando memoria 
    struct b_pixelRGB** RowsPixelsRGB = (struct b_pixelRGB**) calloc(sizeof(struct b_pixelRGB*), HEIGHT);
    struct b_pixelYUV** RowsPixelsYUV = (struct b_pixelYUV**) calloc(sizeof(struct b_pixelYUV*), HEIGHT);
-
-   for (i = 0; i < HEIGHT; ++i)
+   
+   //struct b_pixelRGB b_pixRGB;
+   for (r = 0; r < HEIGHT; ++r)
    {
-      RowsPixelsRGB[i] = (struct b_pixelRGB*) calloc(sizeof(struct b_pixelRGB), WIDTH);
-      RowsPixelsYUV[i] = (struct b_pixelYUV*) calloc(sizeof(struct b_pixelYUV), WIDTH);
-      
-      // Leyendo la imagen completa
-      fread(RowsPixelsRGB[i], sizeof(struct b_pixelRGB), WIDTH, inputFile);
+      RowsPixelsRGB[r] = (struct b_pixelRGB*) calloc(sizeof(struct b_pixelRGB), WIDTH);
+      RowsPixelsYUV[r] = (struct b_pixelYUV*) calloc(sizeof(struct b_pixelYUV), WIDTH);
+     
+      fread(RowsPixelsRGB[r], sizeof(struct b_pixelRGB), WIDTH, inputFile);
+/*
+      for(c = 0; c < WIDTH; c++)
+      {
+         // Leyendo la imagen completa
+         fread(&b_pixRGB, sizeof(struct b_pixelRGB), 1, inputFile);
+
+         // Pasar de 8 bit 0 a 255 a float entre 0 y 1
+         RowsPixelsRGB[r][c].r = ((float32_t) b_pixRGB.r)/MAX_BYTE_VAL;
+         RowsPixelsRGB[r][c].g = ((float32_t) b_pixRGB.g)/MAX_BYTE_VAL;
+         RowsPixelsRGB[r][c].b = ((float32_t) b_pixRGB.b)/MAX_BYTE_VAL;
+         RowsPixelsRGB[r][c].z = 0;
+      }*/
    }
 
    /*
@@ -106,46 +126,52 @@ void rgb2yuv(char *input_image, char *output_image)
    struct b_pixelRGB b_pixRGB;
    struct f_pixelRGB f_pixRGB;
    struct f_pixelYUV f_pixYUV;
-   float tmpYUV;
-   float tmp1;
-   float tmp2;
-   float tmp3;
+   float32x2_t* tmpYUV = (float32x2_t*) &f_pixYUV;
+
+   float32x2_t* pixel;
+
+   float32x2_t* tmpYx2 = (float32x2_t*) calloc(sizeof(float32x2_t), 2);
+   float32x2_t* tmpUx2 = (float32x2_t*) calloc(sizeof(float32x2_t), 2);
+   float32x2_t* tmpVx2 = (float32x2_t*) calloc(sizeof(float32x2_t), 2);
+
+   float32x2_t tmpY;
+   float32x2_t tmpU;
+   float32x2_t tmpV;
+   
+   float32x2_t tmpZ = {0,0};
 
    for(r = 0; r < HEIGHT; r++)
    {
+      //rowPixels = (float32x4_t*) RowsPixelsRGB[r];
       for(c = 0; c < WIDTH; c++)
       {
          b_pixRGB = RowsPixelsRGB[r][c];
 
          // Pasar de 8 bit 0 a 255 a float entre 0 y 1
-         f_pixRGB.r = ((float) b_pixRGB.r)/MAX_BYTE_VAL;
-         f_pixRGB.g = ((float) b_pixRGB.g)/MAX_BYTE_VAL;
-         f_pixRGB.b = ((float) b_pixRGB.b)/MAX_BYTE_VAL;
+         f_pixRGB.r = ((float32_t) b_pixRGB.r)/MAX_BYTE_VAL;
+         f_pixRGB.g = ((float32_t) b_pixRGB.g)/MAX_BYTE_VAL;
+         f_pixRGB.b = ((float32_t) b_pixRGB.b)/MAX_BYTE_VAL;
+         f_pixRGB.z = 0.0;  
 
          // Calculo YUV
-         tmp1 = convertNumbers[0][0]*f_pixRGB.r; 
-         tmp2 = convertNumbers[0][1]*f_pixRGB.g; 
-         tmp3 = convertNumbers[0][2]*f_pixRGB.b;
-         tmpYUV = tmp1 + tmp2;
-         f_pixYUV.y = tmpYUV + tmp3;
+         
+         pixel = (float32x2_t*) (&f_pixRGB); 
 
-         tmp1 = convertNumbers[1][0]*f_pixRGB.r; 
-         tmp2 = convertNumbers[1][1]*f_pixRGB.g; 
-         tmp3 = convertNumbers[1][2]*f_pixRGB.b;
-         tmpYUV = tmp1 + tmp2;
-         f_pixYUV.u = tmpYUV + tmp3;
+         tmpYx2[0] = vmul_f32(pixel[0], coefs[Y][0]);
+         tmpUx2[0] = vmul_f32(pixel[0], coefs[U][0]);
+         tmpVx2[0] = vmul_f32(pixel[0], coefs[V][0]);
 
-         tmp1 = convertNumbers[2][0]*f_pixRGB.r; 
-         tmp2 = convertNumbers[2][1]*f_pixRGB.g; 
-         tmp3 = convertNumbers[2][2]*f_pixRGB.b;
-         tmpYUV = tmp1 + tmp2;
-         f_pixYUV.v = tmpYUV + tmp3;
+         tmpYx2[1] = vmul_f32(pixel[1], coefs[Y][1]);
+         tmpUx2[1] = vmul_f32(pixel[1], coefs[U][1]);
+         tmpVx2[1] = vmul_f32(pixel[1], coefs[V][1]);
 
-/*
-         f_pixYUV.y = convertNumbers[0][0]*f_pixRGB.r + convertNumbers[0][1]*f_pixRGB.g + convertNumbers[0][2]*f_pixRGB.b;
-         f_pixYUV.u = convertNumbers[1][0]*f_pixRGB.r + convertNumbers[1][1]*f_pixRGB.g + convertNumbers[1][2]*f_pixRGB.b;
-         f_pixYUV.v = convertNumbers[2][0]*f_pixRGB.r + convertNumbers[2][1]*f_pixRGB.g + convertNumbers[2][2]*f_pixRGB.b;
-  */ 
+         tmpY = vpadd_f32(tmpYx2[0], tmpYx2[1]);
+         tmpU = vpadd_f32(tmpUx2[0], tmpUx2[1]);
+         tmpV = vpadd_f32(tmpVx2[0], tmpVx2[1]);
+
+         tmpYUV[0] = vpadd_f32(tmpY, tmpU);
+         tmpYUV[1] = vpadd_f32(tmpV, tmpZ);
+         
          // Pasar de escala YUV a 255
          f_pixYUV.y = f_pixYUV.y*MAX_BYTE_VAL;
          f_pixYUV.u = (f_pixYUV.u/(SCALE_U))*MAX_BYTE_VAL+OFFSET_UV;
@@ -157,13 +183,13 @@ void rgb2yuv(char *input_image, char *output_image)
          f_pixYUV.v = to8bits(f_pixYUV.v);
 
          // Pasar YUV de float a 8 bits
-         RowsPixelsYUV[r][c].y = (unsigned char) f_pixYUV.y;
-         RowsPixelsYUV[r][c].u = (unsigned char) f_pixYUV.u;
-         RowsPixelsYUV[r][c].v = (unsigned char) f_pixYUV.v;
+         RowsPixelsYUV[r][c].y = (uint8_t) f_pixYUV.y;
+         RowsPixelsYUV[r][c].u = (uint8_t) f_pixYUV.u;
+         RowsPixelsYUV[r][c].v = (uint8_t) f_pixYUV.v;
       }
    }
 
-   for(int r = 0; r < HEIGHT; r++)
+   for(r = 0; r < HEIGHT; r++)
    {
       fwrite(RowsPixelsYUV[r], sizeof(struct b_pixelYUV), WIDTH, oFile);
    }
@@ -188,29 +214,30 @@ int main (int argc, char **argv)
 {
    char *outputFile = NULL;
    char *inputFile = NULL;
-
+   
    clock_t start, end;
    double cpu_time_used;
 
+   int i;
    int c;
    while ((c = getopt (argc, argv, ":i:o:hat")) != -1)
    {
-      switch(c)
-      {
-         case 'i':
-         {
-            inputFile = optarg;
-            printf("Input file: %s\n", inputFile); 
-            break;
-         }
-         case 'o':
-         {
-            outputFile = optarg;
-            printf("Output file: %s\n", outputFile);
-            break;
-         }
-         case 'h':
-         {
+   	switch(c)
+   	{
+   		case 'i':
+   		{
+   			inputFile = optarg;
+   			printf("Input file: %s\n", inputFile); 
+   			break;
+   		}
+   		case 'o':
+   		{
+   			outputFile = optarg;
+   			printf("Output file: %s\n", outputFile);
+   			break;
+   		}
+   		case 'h':
+   		{
             printf("***************\n*    Usage    * \n***************\n"
                   "\n./rgb2yuv [ -i RGBfile ] [ -o YUVfile ] [ -h ] [ -a ] \n"
                   "-i RGBfile ​ \t specifies the RGB file to be converted.\n"
@@ -218,17 +245,17 @@ int main (int argc, char **argv)
                   "-a \t\t ​displays the information of the author of the program.\n"
                   "-h​ \t\t displays the usage message to let the user know how to execute the application.\n \n");
             break;
-         }
-         case 'a':
-         {
-            printf("Authors:\n Luis Arias \n Guillermo Lopez \n Edward Umana\n");
-            break;
-         }
+   		}
+   		case 'a':
+   		{
+   			printf("Authors:\n Luis Arias \n Guillermo Lopez \n Edward Umana\n");
+   			break;
+   		}
          case 't':
          {
 
             double average = 0.0;
-            for(int i = 0; i < 100; i++)
+            for(i = 0; i < 100; i++)
             {
                start = clock();
 
@@ -243,9 +270,7 @@ int main (int argc, char **argv)
             printf("\nAverage %fs\n", cpu_time_used);
             break;
          }
-
-
-      }
+   	}
    }
 
    start = clock();
